ef94a20a88b79a86b2a3b19cd72b42c5fde0ce2d
Marcola Garcia <akashwebroot@gmail.com>
2024-02-20 14:32:02 +0530

    Header and footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,53 @@
-<h2>footer</h2>
+{{-- FOOTER --}}
+
+<div class="mainfooter">
+    <div class="footerinner">
+        <div class="row justify-content-between">
+            <div class="col-md-3">
+                <img src="{{ url('images/logoNgo.png') }}" alt="">
+                <h3>About Us</h3>
+                <p class="footercontn">Youngistaan Foundation is a network of volunteers, committed to making a difference in the community by helping those in need.</p>
+            </div>
+            <div class="col-md-3">
+                <h3>Recent Post</h3>
+                <ul class="blogsList">
+                    <li>
+                        <h4>Every cloud has a silver lining</h4>
+                        <p class="date">July 25, 2018</p>
+                    </li>
+                    <li>
+                        <h4>Every cloud has a silver lining</h4>
+                        <p class="date">July 25, 2018</p>
+                    </li>
+                    <li>
+                        <h4>Every cloud has a silver lining</h4>
+                        <p class="date">July 25, 2018</p>
+                    </li>
+                    <li>
+                        <h4>Every cloud has a silver lining</h4>
+                        <p class="date">July 25, 2018</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h3>Category List</h3>
+                <ul class="charityList">
+                    <li><h4>Charity (1)</h4></li>
+                    <li><h4>Children Stories (4)</h4></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+               <h3>Quick Search</h3>
+               <ul class="quick">
+                <li><h4>Log in</h4></li>
+                <li><h4>Entries feed</h4></li>
+                <li><h4>Comments feed</h4></li>
+                <li><h4>wordpress.org</h4></li>
+               </ul>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <p>Copyright © 2021 Youngistaan by SM Technologies India. All Rights Reserved.</p>
+    </div>
+</div>
