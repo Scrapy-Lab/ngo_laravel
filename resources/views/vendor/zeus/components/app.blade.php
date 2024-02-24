0c0b609b2ef61340b562ceb0f6d21acb231c1c41
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-02-24 12:21:51 +0530

    blog

--- resources/views/vendor/zeus/components/app.blade.php
+++ resources/views/vendor/zeus/components/app.blade.php
@@ -18,8 +18,10 @@
     @filamentStyles
     @stack('styles')
 
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('vendor/zeus/frontend.css') }}">
-
     <style>
         * {font-family: 'KoHo', 'Almarai', sans-serif;}
         [x-cloak] {display: none !important;}
@@ -27,7 +29,7 @@
 </head>
 <body class="font-sans antialiased bg-gray-50 text-gray-900 dark:text-gray-100 dark:bg-gray-900 @if(app()->isLocal()) debug-screens @endif">
 
-<header x-data="{ open: false }" class="bg-white dark:bg-black px-4">
+{{-- <header x-data="{ open: false }" class="bg-white dark:bg-black px-4">
     <div class="container mx-auto">
         <div class="flex justify-between h-16">
             <div class="flex">
@@ -39,48 +41,49 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex sm:items-center">
-                    {{--Navigation Links--}}
+                    Navigation Links
                 </div>
 
             </div>
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-                {{--Account menu and other icons--}}
+                Account menu and other icons
             </div>
         </div>
     </div>
-</header>
+</header> --}}
+@include('layouts.header')
 
-<header class="bg-gray-100 dark:bg-gray-800">
-        <div class="container mx-auto py-2 px-3">
-            <div class="flex justify-between items-center">
-                <div class="w-full">
-                    @if(isset($breadcrumbs))
-                        <nav class="text-gray-400 font-bold my-1" aria-label="Breadcrumb">
-                            <ol class="list-none p-0 inline-flex">
-                                {{ $breadcrumbs }}
-                            </ol>
-                        </nav>
-                    @endif
-                    @if(isset($header))
-                        <div class="italic font-semibold text-xl text-gray-600 dark:text-gray-100">
-                            {{ $header }}
-                        </div>
-                    @endif
+    <header class="bg-gray-100 dark:bg-gray-800">
+            <div class="container mx-auto py-2 px-3">
+                <div class="flex justify-between items-center">
+                    <div class="w-full">
+                        @if(isset($breadcrumbs))
+                            <nav class="text-gray-400 font-bold my-1" aria-label="Breadcrumb">
+                                <ol class="list-none p-0 inline-flex">
+                                    {{ $breadcrumbs }}
+                                </ol>
+                            </nav>
+                        @endif
+                        @if(isset($header))
+                            <div class="italic font-semibold text-xl text-gray-600 dark:text-gray-100">
+                                {{ $header }}
+                            </div>
+                        @endif
+                    </div>
+                    <span class="bolt-loading animate-pulse"></span>
                 </div>
-                <span class="bolt-loading animate-pulse"></span>
             </div>
-        </div>
-    </header>
+        </header>
 
-<div class="container mx-auto my-6">
-    {{ $slot }}
-</div>
+    <div class="container mx-auto my-6">
+        {{ $slot }}
+    </div>
 
-<footer class="bg-gray-100 dark:bg-gray-800 p-6 text-center font-light">
-    <a href="https://larazeus.com" target="_blank">
-        a gift with ❤️ &nbsp;from @zeus
-    </a>
-</footer>
+    <footer class="bg-gray-100 dark:bg-gray-800 p-6 text-center font-light">
+        <a href="https://larazeus.com" target="_blank">
+            a gift with ❤️ &nbsp;from @zeus
+        </a>
+    </footer>
 
 @stack('scripts')
 @livewireScripts
