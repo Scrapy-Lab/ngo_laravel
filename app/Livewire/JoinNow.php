78d59af1c2bfce343d194411c45dd721d6956ba7
dev-dhull <dev.vikasdhull@gmail.com>
2024-03-12 19:43:11 +0530

    join now page

---
+++ app/Livewire/JoinNow.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class JoinNow extends Component
+{
+    public function render()
+    {
+        return view('livewire.join-now');
+    }
+
+    public function joinNow()
+    {
+        
+    }
+}
