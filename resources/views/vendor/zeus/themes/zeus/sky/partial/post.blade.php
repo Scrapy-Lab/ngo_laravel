e9ece1bd7ce93451bc274d2ae2c1c279548baaa9
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-03-21 23:08:07 +0530

    time added in blog

--- resources/views/vendor/zeus/themes/zeus/sky/partial/post.blade.php
+++ resources/views/vendor/zeus/themes/zeus/sky/partial/post.blade.php
@@ -1,7 +1,7 @@
 <article class="mt-6" itemscope itemtype="https://schema.org/Movie">
     <div class="px-6 pb-6 mx-auto bg-white dark:bg-gray-800 rounded-[2rem] rounded-bl-none rounded-tr-none shadow-md">
         <div class="flex items-center justify-between">
-            <span class="font-light text-sm text-gray-600 dark:text-gray-200 mt-2">{{ optional($post->published_at)->diffForHumans() ?? '' }}</span>
+            {{-- <span class="font-light text-sm text-gray-600 dark:text-gray-200 mt-2">{{ optional($post->published_at)->diffForHumans() ?? '' }}</span> --}}
             <div>
                 @unless ($post->tags->isEmpty())
                     @each($skyTheme.'.partial.category', $post->tags->where('type','category'), 'category')
@@ -21,7 +21,16 @@
             <a href="{{ route('post',$post->slug) }}" class="blogMaintitle text-2xl md:text-3xl font-bold text-gray-700 dark:text-gray-200 hover:underline">
                 {!! $post->title !!}
             </a>
-            {{-- <br> <span class="font-light text-sm text-gray-600 dark:text-gray-200 mt-2">{{ optional($post->published_at)->diffForHumans() ?? '' }}</span> --}}
+            <div class="d-flex">
+
+                <img class="paddingDateLogo" src="{{asset('images/logo/dateLogo.png')}}" alt="" width="35px">
+                <span class="font-light text-sm text-gray-600 dark:text-gray-200 mt-2 paddingDateLogo pr-2">{{ optional($post->published_at)->diffForHumans() ?? '' }}</span>
+                <a class="flex items-center gap-2">
+                    <img src="{{ \Filament\Facades\Filament::getUserAvatarUrl($post->author) }}" alt="avatar" class="hidden object-cover w-8 h-8 rounded-full sm:block ">
+                    <p class="text-gray-700 dark:text-gray-200 hover:underline">{{ $post->author->name ?? '' }}</p>
+                </a>
+            </div>
+            <br>
             @if($post->description !== null)
                 <p class="mt-2 text-gray-600 dark:text-gray-200">
                     {!! $post->description !!}
@@ -31,10 +40,7 @@
         <div class="flex items-center justify-between mt-4">
             <a href="{{ route('post',$post->slug) }}" class=" blogMainButton text-blue-500 dark:text-blue-200 hover:underline">Read more</a>
             <div>
-                <a class="flex items-center gap-2">
-                    <img src="{{ \Filament\Facades\Filament::getUserAvatarUrl($post->author) }}" alt="avatar" class="hidden object-cover w-8 h-8 rounded-full sm:block">
-                    <p class="text-gray-700 dark:text-gray-200 hover:underline">{{ $post->author->name ?? '' }}</p>
-                </a>
+
             </div>
         </div>
     </div>
