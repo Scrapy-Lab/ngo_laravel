d7c2fe7a1b50a9e7035892145aca7f88afb212b1
D Luffy <akashwebroot@gmail.com>
2024-04-04 22:45:16 +0530

    changesResponsive and other

--- resources/views/livewire/donate-form.blade.php
+++ resources/views/livewire/donate-form.blade.php
@@ -61,7 +61,7 @@
                       " wire:click="fixedAmount(200)">
                             <span class="donateAmountButton"> ₹ 200</span>
                         </div>
-                        <div class="col-md-2
+                        <div class="mobiRate col-md-2
                       " wire:click="fixedAmount(500)">
                             <span class="donateAmountButton"> ₹ 500</span>
                         </div>
