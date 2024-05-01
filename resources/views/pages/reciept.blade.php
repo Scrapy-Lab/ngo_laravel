eeea3ced17bdcc8dca74ec1d967c4c5aa4c11f40
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-05-01 23:22:25 +0530

    reciept design update

--- resources/views/pages/reciept.blade.php
+++ resources/views/pages/reciept.blade.php
@@ -19,7 +19,7 @@
     <div style="max-width: 1020px;width: 100%;position: relative;margin: 0 auto;padding: 10px 0;">
         <div style="display: flex;justify-content: space-between;align-items: center;margin: 0 26px;">
             <img src="{{ public_path('images/mail_images/logo.png') }}" w="20" h="20" alt="logo" style="width: 150px;height: 120px"/>
-            <div style="width: 50%; margin-left:300px ; margin-top:-130px">
+            <div style="width: 80%; margin-left:200px ; margin-top:-130px">
                 <h1 style="color: #cc2121;font-size: 22px;font-family: sans-serif;font-weight: 700;">YOUNGISTAAN WELFARE SOCIETY  (REGD.)  </h1>
                 <span>Regd. No. 201721103 | Society ACT  (XXI 1860)  | ESTD. 22/Feb/2017  </span>
                 <p>Head Office Address : #1123, 2nd Floor, Sector-39, Opp. ICICI Bank, Chandigarh Road, Ludhiana -
